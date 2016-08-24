efb8c28b7b6e64576ab3fbd580a38eafdfea697a
leesuckgeun <beast0460@gmail.com>
2016-08-24 09:08:09 +0900

    refactoring docstring

--- nnmodule.py
+++ nnmodule.py
@@ -8,23 +8,24 @@ class Network(object):
     """
 
     def __init__(self, architecture):
-        """
+        '''
         Initialize Network.
-
+        
         as parameter, only gets three valued list, which is 
         [input layer, hidden layer, output layer] form. 
         
-        Creates biases for hidden layer and output layer.
-        Note that input layer doesn't have biases. 
+        1. Creates biases for hidden layer and output layer.
+            Note that input layer doesn't have biases. 
         
-        Creates weights for (input-hidden), (hidden-output) connections. 
-
-        @type  architecture: list 
-        @param architecture: Architecture of the neural network. 
-                             Must be [ number of units in input layer,
+        2. Creates weights for (input-hidden), (hidden-output) connections. 
+        
+        
+        :param architecture: list
+        :type architecture: Architecture of the neural network. 
+                            Must be [ number of units in input layer,
                                        number of units in hidden layer, 
                                        number of units in output layer ] 
-        """
+        '''
         self.num_layers = len(architecture)
         self.architecture = architecture
         
@@ -41,36 +42,59 @@ class Network(object):
     
     def SGD(self, training_data, num_epochs, mini_batch_size, learn_rate,
             test_data=None):
+        '''
+        Train the neural network using mini-batch stochastic gradient descent. 
         
-        """Train the neural network using mini-batch stochastic
-        gradient descent.  The "training_data" is a list of tuples
-        "(x, y)" representing the training inputs and the desired
-        outputs.  The other non-optional parameters are
-        self-explanatory.  If "test_data" is provided then the
+        If "test_data" is provided then the
         network will be evaluated against the test data after each
         epoch, and partial progress printed out.  This is useful for
-        tracking progress, but slows things down substantially."""
+        tracking progress, but slows things down substantially.
         
+        
+        :param training_data: list of tuples "(x, y)" representing the 
+                        training inputs and the desired outputs. 
+        :type training_data: list of tuples
+        :param num_epochs: number of epochs to train 
+        :type num_epochs: int
+        :param mini_batch_size: mini batch size
+        :type mini_batch_size: int 
+        :param learn_rate: learning rate
+        :type learn_rate: float
+        :param test_data: testing data
+        :type test_data: 
+        '''        
         for i in xrange(num_epochs):
             
-            mini_batches = create_mini_batches(training_data, mini_batch_size)
+            mini_batches = self.create_mini_batches(
+                    training_data, mini_batch_size)
             
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, learn_rate)
             
             if test_data is not None:
+                num_test = len(test_data)
                 num_corret = self.evaluate(test_data)
-                print "Epoch {0}: {1} / {2}".format(
-                    i, num_corret, len(test_data))
+                print "Epoch {0}: {1} / {2}".format(i, num_corret, num_test)
             else:
                 print "Epoch {0} complete".format(i)
+                
+    def create_mini_batches(self, data, size):
+        """Creates Mini batches given data and size"""
+        return [ data[i:i+size] for i in xrange(0, len(data), size) ]
     
     
     def update_mini_batch(self, mini_batch, learn_rate):
-        """Update the network's weights and biases by applying
+        '''
+        Update the network's weights and biases. 
+        
+        Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
-        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``learn_rate``
-        is the learning rate."""
+        
+        :param mini_batch: mini batch
+        :type mini_batch: list of tuples ``(x, y)``
+        :param learn_rate: learning rate
+        :type learn_rate: float 
+        '''
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
@@ -88,7 +112,8 @@ class Network(object):
         network outputs the correct result. Note that the neural
         network's output is assumed to be the index of whichever
         neuron in the final layer has the highest activation."""
-        test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
+        test_results = [(np.argmax(self.feedforward(x)), y) 
+                        for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
 
     
@@ -108,9 +133,6 @@ def sigmoid_prime(x):
     """Derivative of the sigmoid function"""
     return sigmoid(x)*(1-sigmoid(x))
 
-def create_mini_batches(data, size):
-    """Creates Mini batches given data and size"""
-    return [data[i:i+size] for i in xrange(0, len(data), size)]
 
 
 
